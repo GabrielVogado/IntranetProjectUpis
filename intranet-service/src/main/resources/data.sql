fbfa813d9577c02a90f81deeaa1f4f4a439329da
unknown <gabriel.vogado@gmail.com>
2018-10-04 03:17:13 -0300

    TESTE

---
+++ intranet-service/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into Colaborador values('aaaaa')
---
+++ intranet-service/src/test/java/com/upisintranet/intranet/IntranetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.upisintranet.intranet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntranetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
